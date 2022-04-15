8b2445b87c20f536b1996430b17ff0b9f9e3271e
sprout110 <32226315+sprout110@users.noreply.github.com>
2022-04-15 17:30:23 +0800

    增加 apple-touch-icon

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
+    <link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
     <title>Hello, world!</title>
   </head>
   <body>
